3fbb157733d4e0609fb99077dc78caf518b381df
Aaron Snoswell <aaronsnoswell@gmail.com>
2021-01-29 10:38:15 +1000

    Add missing environment imports

--- mdp_extras/envs/__init__.py
+++ mdp_extras/envs/__init__.py
@@ -1,4 +1,5 @@
 """Move imports to module level for convenience"""
 
-from .nchain import nchain_extras
-from .frozen_lake import frozen_lake_extras
+from .nchain import *
+from .frozen_lake import *
+from .taxi import *
