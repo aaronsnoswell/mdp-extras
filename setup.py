0de54403f4886e1140caac98044279325f3db0d9
Aaron Snoswell <aaronsnoswell@fastmail.com>
2021-05-13 17:11:35 +1000

    Update requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ from setuptools import setup, find_packages
 setup(
     name="mdp_extras",
     version="0.0.1",
-    install_requires=["numpy",],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "gym",
+        "numba",
+    ],
     packages=find_packages(),
 )
